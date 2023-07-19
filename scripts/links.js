7a616461837bd21dbce88d22b02aa8d239710756
kathy-BYUI <cam21026@byui.edu>
2023-07-19 15:23:50 -0500

    weather and links

--- scripts/links.js
+++ scripts/links.js
@@ -2,10 +2,32 @@ const baseURL = "https://kathy-BYUI.github.io/wdd230/";
 
 const linksURL = "https://kathy-BYUI.github.io/wdd230/data/links.json";
 
-async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    console.log(data);
-  }
-  
-getLinks();
+async function getLinks(linksURL) {
+    try {
+        const response = await fetch(linksURL);
+        if (!response.ok) {
+            throw new Error('Failed to fetch links data');
+        }
+        const data = await response.json();
+        const { weeks } = data;
+        displayLinks(weeks);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getLinks(linksURL);
+
+const weeksList = document.querySelector("#act-title + ul");
+
+function displayLinks(weeks) {
+    weeks.forEach((weekData) => {
+        const { week, links } = weekData;
+        const listItem = document.createElement("li");
+        const listItemContent = document.createElement("a");
+        const linksHTML = links.map((link) => `<a href="${link.url}">${link.title}</a>`).join(" | ");
+        listItemContent.innerHTML = `${week}: ${linksHTML}`;
+        listItem.appendChild(listItemContent);
+        weeksList.appendChild(listItem);
+    });
+}
--- scripts/weather.js
+++ scripts/weather.js
@@ -1,8 +1,9 @@
-const currentTemp = document.querySelector('#current-temp');
+const cityTemp = document.querySelector('#city-temperature');
+const currentTemp = document.querySelector('#current-temperature');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&units=imperial&appid=390c7e0fc300a323e003ec765f111f38";
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=-9.37&lon=-75.03&units=imperial&appid=390c7e0fc300a323e003ec765f111f38";
 
 async function apiFetch(url) {
   try {
@@ -20,11 +21,12 @@ async function apiFetch(url) {
 }
 
 function displayResults(data) {
-  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  cityTemp.innerHTML = `${data.name}, ${data.sys.country}`;
+  currentTemp.innerHTML = `${data.main.temp}&deg;F `;
   const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
   let desc = data.weather[0].description;
   weatherIcon.setAttribute('src', iconsrc);
   weatherIcon.setAttribute('alt', desc);
-  captionDesc.textContent = `${desc}`;
+  captionDesc.textContent = ` - ${desc}`;
 }
 apiFetch(url);
